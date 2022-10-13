7222d8cce6e4e5ecb4e48d135ce9681ced0e9122
Alex Lee <dung.le@snapmart.ph>
2022-10-14 00:52:44 +0700

    Total Reward

--- containers/Home/index.tsx
+++ containers/Home/index.tsx
@@ -10,18 +10,18 @@ const Home: FC = () => {
       {/* REWARD */}
 
       <div className="bg-pcgray rounded-xl flex flex-col lg:flex-row justify-around p-10 lg:gap-40 gap-2 ">
-        <div className=" gap-2 justify-center">
+        <div className="flex flex-col gap-1 justify-center items-start text-left">
           <div className="text-content">Total Reward</div>
           <div className="text-xl lg:text-3xl font-semibold">$8.064.000</div>
           <Image
             src="/images/btn_view_contract.svg"
             alt=""
-            height={24}
+            height={20}
             width={120}
-            className="cursor-pointer mx-auto "
+            className="cursor-pointer mx-auto 100 "
           />
         </div>
-        <div className="flex flex-col  gap-2">
+        <div className="flex flex-col  gap-1">
           <div className="text-content">Participant</div>
           <div className="text-xl lg:text-3xl font-semibold">40.320</div>
         </div>
@@ -29,7 +29,7 @@ const Home: FC = () => {
 
       {/* HERO IMAGES */}
       <div className="flex flex-col lg:flex-row  justify-between bg-pcgray rounded-md">
-        <div className='w-full lg:w-7/12 flex items-center'>
+        <div className="w-full lg:w-7/12 flex items-center">
           <img
             src="/images/hero_football_2022.png"
             alt=""
