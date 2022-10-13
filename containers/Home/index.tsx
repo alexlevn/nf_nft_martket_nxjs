1a5e69ed76ac9507898e8eb8559c28ea17097208
Alex Lee <dung.le@snapmart.ph>
2022-10-14 00:01:34 +0700

    save

--- containers/Home/index.tsx
+++ containers/Home/index.tsx
@@ -2,14 +2,15 @@
 import Image from 'next/image'
 import { FC } from 'react'
 import MintNFT from 'components/MintNFT'
+import { ButtonBorderGradient } from 'components/ButtonBorderGradient'
 
 const Home: FC = () => {
   return (
     <div className="text-xl font-light  bg-dark min-h-screen p-0 flex flex-col gap-5 ">
       {/* REWARD */}
 
-      <div className="flex flex-col lg:flex-row justify-around rounded-xl  lg:gap-40 gap-2 bg-red-500 p-30">
-      <div className=" flex flex-col gap-2 justify-center">
+      <div className="bg-pcgray rounded-xl flex flex-col lg:flex-row justify-around p-10 lg:gap-40 gap-2 ">
+        <div className=" gap-2 justify-center">
           <div className="text-content">Total Reward</div>
           <div className="text-xl lg:text-3xl font-semibold">$8.064.000</div>
           <Image
@@ -19,9 +20,7 @@ const Home: FC = () => {
             width={120}
             className="cursor-pointer mx-auto "
           />
-          <div className="bg-red-100">hello</div>
         </div>
-
         <div className="flex flex-col  gap-2">
           <div className="text-content">Participant</div>
           <div className="text-xl lg:text-3xl font-semibold">40.320</div>
@@ -30,18 +29,65 @@ const Home: FC = () => {
 
       {/* HERO IMAGES */}
       <div className="flex flex-col lg:flex-row  justify-between bg-pcgray rounded-md">
-        <img
-          src="/images/hero_football_2022.png"
-          alt=""
-          className="rounded-md"
-        />
+        <div>
+          <img
+            src="/images/hero_football_2022.png"
+            alt=""
+            className="rounded-md"
+          />
+        </div>
         <MintNFT />
       </div>
-      <div className='text-red-500'>Hello</div>
     </div>
-
-    
   )
 }
 
 export default Home
+
+const a = (
+  <div className="text-xl font-light  bg-dark min-h-screen p-0 flex flex-col gap-5">
+    <div className="bg-pcgray rounded-xl flex flex-col lg:flex-row justify-around p-10 lg:gap-40 gap-2 ">
+      <div className=" gap-2 justify-center">
+        <div className="text-content">Total Reward</div>
+        <div className="text-xl lg:text-3xl font-semibold">$8.064.000</div>
+        <Image
+          src="/images/btn_view_contract.svg"
+          alt=""
+          height={24}
+          width={120}
+          className="cursor-pointer mx-auto "
+        />
+      </div>
+      <div className="flex flex-col  gap-2">
+        <div className="text-content">Participant</div>
+        <div className="text-xl lg:text-3xl font-semibold">40.320</div>
+      </div>
+    </div>
+
+    <div className="flex flex-col lg:flex-row  justify-between bg-pcgray rounded-md">
+      <img src="/images/hero_football_2022.png" alt="" className="rounded-md" />
+
+      <div className="flex flex-col gap-5 p-5 lg:p-8  justify-between">
+        <div className="flex flex-col gap-5">
+          <p className="text-white font-semibold">Mint Your NFT</p>
+          <p className="text-content">
+            Mint randomly releases NFT shoes to represent the national team
+          </p>
+          <div className="flex justify-between">
+            <span className="text-content">Minted times: </span>
+            <span className="text-white font-semibold text-sm">0</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-content">Minted rate: </span>
+            <span className="text-white font-semibold text-sm">
+              20% Get Tier1
+            </span>
+          </div>
+        </div>
+        <ButtonBorderGradient>
+          <span className="text-base">Connect Wallet</span>
+        </ButtonBorderGradient>
+      </div>
+    </div>
+  </div>
+)
