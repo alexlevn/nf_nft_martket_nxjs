cde84e3afcc9a3a85690a4c64c2a64c942b20de2
Alex Lee <dung.le@snapmart.ph>
2022-10-25 09:31:40 +0700

    done design

--- components/MintNFT/index.tsx
+++ components/MintNFT/index.tsx
@@ -1,20 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import { useCallback, useState } from 'react'
 import useWeb3 from 'common/hooks/useWeb3'
 import { ButtonBorderGradient } from 'components/ButtonBorderGradient'
 import { ButtonGradient } from 'components/ButtonGradient'
+import ModalTrigger from 'components/ModalTrigger'
+import { Modal, Spin } from 'antd'
 
 const MintNFT = () => {
   const { connected, connect, allowance, approve, mint } = useWeb3()
-  console.log(allowance);
-  
-  const [loadingApprove, setLoadingApprove] = useState(false);
-  const [loadingMint, setLoadingMint] = useState(false);
+  console.log(allowance)
+
+  const [loadingApprove, setLoadingApprove] = useState(false)
+  const [loadingMint, setLoadingMint] = useState(false)
 
   const callbackApprove = useCallback(() => {
     setLoadingApprove(false)
   }, [])
-  
+
   const onClickApprove = useCallback(() => {
+    // console.log('loading approve ...')
+    setLoadingApprove(true)
     approve(callbackApprove)
   }, [approve, callbackApprove])
 
@@ -23,23 +28,116 @@ const MintNFT = () => {
   }, [])
 
   const onClickMint = useCallback(() => {
+    console.log('loading mint = true ...')
+
     setLoadingMint(true)
-    mint(callbackMint)
+    setTimeout(() => {
+      mint(callbackMint)
+    }, 3000)
   }, [mint, callbackMint])
 
   const renderBtn = useCallback(() => {
     if (connected()) {
+      // --- design
+      return (
+        <>
+          <div>allowance: {allowance.toString()}</div>
+          <ButtonGradient onClick={onClickMint} className="py-2 text-base">
+            <span>TEST </span>
+          </ButtonGradient>
+          <Modal
+            width={400}
+            footer={null}
+            closeIcon={null}
+            open={true}
+            onCancel={(_) => {}}
+            className="bg-transparent"
+          >
+            <div className="text-white flex-center flex flex-col">
+              <ButtonBorderGradient>
+                <div className="flex-center">
+                  <img
+                    src="/images/shoes/shoe_brazil.png"
+                    alt=""
+                    className="cursor-auto h-60 w-60 lg:w-80 lg:h-80"
+                  />
+                </div>
+                <div className="flex flex-wrap w-full text-base">
+                  <div className="flex flex-col w-1/2 px-10 py-3 gap-1">
+                    <span className="text-scgray_4">Team</span>
+                    <span className="text-white">Brazil</span>
+                  </div>
+                  <div className="flex flex-col w-1/2 px-10 py-3 gap-1">
+                    <span className="text-scgray_4">Rarity</span>
+                    <span className="text-white">0,08%</span>
+                  </div>
+                  <div className="flex flex-col w-1/2 px-10 py-3 gap-1">
+                    <span className="text-scgray_4">Estimate</span>
+                  </div>
+                  <div className="flex flex-col w-1/2 px-10 py-3 gap-1">
+                    <span className="text-white">≈ $68,000</span>
+                  </div>
+                </div>
+              </ButtonBorderGradient>
+            </div>
+            <div className="mt-5">
+              <ButtonGradient onClick={() => {}} className="py-3 text-lg ">
+                Collect
+              </ButtonGradient>
+            </div>
+            <div className="mt-5">
+              <ButtonBorderGradient
+                onClick={() => {}}
+                className="py-3 text-lg flex-center"
+              >
+                <span>View on BSCscan</span>
+              </ButtonBorderGradient>
+            </div>
+          </Modal>
+        </>
+      )
+      // --- end design
       if (allowance) {
         return (
-          <ButtonGradient onClick={onClickMint} className="py-2 text-base">
-            <span>Mint NFT</span>
-          </ButtonGradient>
+          <>
+            <div>allowance: {allowance.toString()}</div>
+            <ButtonGradient onClick={onClickMint} className="py-2 text-base">
+              <span>Mint NFT</span>
+            </ButtonGradient>
+            <Modal
+              width={300}
+              footer={null}
+              closeIcon={null}
+              open={loadingMint}
+              onCancel={(_) => {}}
+            >
+              <div className="h-48 text-white flex-center">minting ...</div>
+            </Modal>
+          </>
         )
       } else {
         return (
-          <ButtonGradient onClick={onClickApprove} className="py-2 text-base">
-            <span>Approve</span>
-          </ButtonGradient>
+          <>
+            <div>allowance: {allowance.toString()}</div>
+            <ButtonGradient onClick={onClickApprove} className="py-2 text-base">
+              <span>Approve</span>
+            </ButtonGradient>
+            <Modal
+              width={300}
+              footer={null}
+              closeIcon={null}
+              open={loadingApprove}
+              onCancel={(_) => {}}
+            >
+              <div className="h-48 text-white flex-center">
+                <img
+                  src="/images/loading.svg"
+                  alt=""
+                  className="w-20 h-20 spin"
+                />
+              </div>
+            </Modal>
+          </>
         )
       }
     } else {
