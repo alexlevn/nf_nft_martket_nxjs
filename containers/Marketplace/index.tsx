04546cfdc0d7c7c79fc84dc992ffa3c842962d4b
Alex Lee <dung.le@snapmart.ph>
2022-10-26 16:05:44 +0700

    shoe card 1 vs shoe card 2

--- containers/Marketplace/index.tsx
+++ containers/Marketplace/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import { ButtonBorderGradient } from 'components/ButtonBorderGradient'
+import { FC } from 'react'
 
 const Marketplace = () => {
   const total = 6969
   return (
     <div className="">
       {/* Total Volume */}
+
       {/* Filter Form */}
       <div className="flex flex-col lg:flex-row justify-between  items-start gap-5">
         <span className="font-semibold">Items (Total {total})</span>
@@ -19,26 +21,17 @@ const Marketplace = () => {
             Filter
             <img src="/images/filter.svg" alt="" className="h-3 w-3" />
           </div>
-          <div className="border-gradient">
-            Filter
-            <img src="/images/filter.svg" alt="" className="h-3 w-3" />
-          </div>
         </div>
       </div>
 
       {/* Table */}
-
       <div className="flex flex-col lg:flex-row items-center flex-wrap gap-5 mt-10 ">
-        {/* {Array.from(Array(10).keys()).map((item) => (
-          <ShoeDetail_2 key={item} />
-        ))} */}
-
-        <ShoeDetail_2 />
-        <ShoeDetail />
+        {Array.from(Array(10).keys()).map((item, index) => (
+          <ShoeDetail key={item} tier={index} />
+        ))}
       </div>
 
       {/* Load More */}
-
       <div className="flex-center mt-10">
         <ButtonBorderGradient className="px-10 py-3">
           Load more
@@ -48,62 +41,54 @@ const Marketplace = () => {
   )
 }
 
-const ShoeDetail = () => (
-  <div className="text-white flex-center flex flex-col bg-scgray rounded-md max-w-sm lg:max-w-xs">
-    <ButtonBorderGradient className="flex flex-col gap-5">
-      <div className="flex-center">
-        <img src="/images/shoes/shoe_brazil.png" alt="" className="h-80 w-80" />
-      </div>
-      <div className="flex flex-wrap w-full text-sm font-light">
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Team</span>
-          <span className="text-white">Brazil</span>
-        </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Rarity</span>
-          <span className="text-white">0,08%</span>
-        </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Estimate</span>
-        </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-white font-bold flex gap-1 items-center">
-            <img src="/images/busd.svg" alt="" className="w-6 h-6" />
-            68,000
-          </span>
-        </div>
-      </div>
-    </ButtonBorderGradient>
-  </div>
-)
+const ShoeDetail: FC<{ tier?: number }> = ({ tier }) => {
+  const arrayBorders = [
+    'border-gradient',
+    'border border-pcblue',
+    'border border-pcyellow',
+    'border border-pcgray_2',
+  ]
 
-const ShoeDetail_2 = () => (
-  <div className="max-w-sm lg:max-w-xs bg-gray-400 rounded-md">
-    <div className="card-nft border-gradient">
-      <div className="flex-center">
-        <img src="/images/shoes/shoe_brazil.png" alt="" className="h-80 w-80" />
-      </div>
-      <div className="flex flex-wrap w-full text-sm font-light">
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Team</span>
-          <span className="text-white">Brazil</span>
+  const border =
+    tier === undefined || arrayBorders[tier] === undefined
+      ? arrayBorders[3]
+      : arrayBorders[tier]
+
+  return (
+    <div className="max-w-sm lg:max-w-xs bg-gray-400 rounded-md">
+      <div className={'card-nft' + ' ' + border}>
+        {/* IMAGE */}
+        <div className="flex-center">
+          <img
+            src="/images/shoes/shoe_brazil.png"
+            alt=""
+            className="h-80 w-80"
+          />
         </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Rarity</span>
-          <span className="text-white">0,08%</span>
-        </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-scgray">Estimate</span>
-        </div>
-        <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
-          <span className="text-white font-bold flex gap-1 items-center">
-            <img src="/images/busd.svg" alt="" className="w-6 h-6" />
-            68,000
-          </span>
+
+        {/* INFO */}
+        <div className="flex flex-wrap w-full text-sm font-light">
+          <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
+            <span className="text-scgray">Team</span>
+            <span className="text-white">Brazil</span>
+          </div>
+          <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
+            <span className="text-scgray">Rarity</span>
+            <span className="text-white">0,08%</span>
+          </div>
+          <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
+            <span className="text-scgray">Price</span>
+          </div>
+          <div className="flex flex-col w-1/2 px-5 py-3 gap-1">
+            <span className="text-white font-bold flex gap-1 items-center">
+              <img src="/images/busd.svg" alt="" className="w-6 h-6" />
+              68,000
+            </span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Marketplace
